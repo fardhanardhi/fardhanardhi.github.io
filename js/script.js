f989e8256d367818085f1daa62376d35ab5a0027
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-10-11 17:34:53 +0700

    update hero page

--- js/script.js
+++ js/script.js
@@ -1,43 +1,51 @@
 $(document).ready(function () {
-  hideAll();
+	hideAll();
 
-  function hideAll() {
-    $('#one').hide();
-    $('#two').hide();
-    $('#three').hide();
-    $('#four').hide();
-    $('#five').hide();
-  }
-  // $('#main').show();
+	function hideAll() {
+		$('#one').hide();
+		$('#two').hide();
+		$('#three').hide();
+		$('#four').hide();
+		$('#five').hide();
+	}
+	// $('#main').show();
 
-  // $('.part').mouseover(function () {
-  //   alert(this.id);
-  // });
+	// $('.part').mouseover(function () {
+	//   alert(this.id);
+	// });
 
-  $('.tags').hover(
-    function () {
-      console.log('hovering on', $(this).attr('id'));
+	$('#brand').hover(
+		function () {
+			console.log('hovering on', $(this).attr('id'));
+			$('#one').show();
+		},
+		function () {
+			hideAll();
 
-      // if ($(this).attr('id') == 'about') {
-      //   $('#one').show();
-      // }
-      if ($(this).attr('id') == 'experience') {
-        $('#two').show();
-      }
-      else if ($(this).attr('id') == 'education') {
-        $('#three').show();
-      }
-      else if ($(this).attr('id') == 'skills') {
-        $('#four').show();
-      }
-      else if ($(this).attr('id') == 'interest') {
-        $('#five').show();
-      }
-    },
-    function () {
-      hideAll();
+		}
+	);
 
-    }
-  );
+	$('.quad').hover(
+		function () {
+			console.log('hovering on', $(this).attr('id'));
+
+			if ($(this).find('.tags').attr('id') == 'experience') {
+				$('#two').show();
+			}
+			else if ($(this).find('.tags').attr('id') == 'education') {
+				$('#three').show();
+			}
+			else if ($(this).find('.tags').attr('id') == 'skills') {
+				$('#four').show();
+			}
+			else if ($(this).find('.tags').attr('id') == 'interest') {
+				$('#five').show();
+			}
+		},
+		function () {
+			hideAll();
+
+		}
+	);
 
 });
